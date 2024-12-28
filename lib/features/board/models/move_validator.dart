6dc227982b36fc654a73db74ac9711602d35ec18
TUANNGUYENPC\tuann <tuannguyenquocios@gmail.com>
2024-12-28 15:30:34 +0700

    update move_validator

--- lib/features/board/models/move_validator.dart
+++ lib/features/board/models/move_validator.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:zenchess/features/board/bloc/board_bloc.dart';
 
 import 'board_model.dart';
@@ -21,6 +23,10 @@ class MoveValidator {
     switch (from.piece!.type) {
       case PieceType.pawn:
         return _isValidPawnMove(from, to, board);
+      case PieceType.rook:
+        return _isValidRookMove(from, to, board);
+      case PieceType.bishop:
+        return _isValidBishopMove(from, to, board);
       default:
         break;
     }
@@ -71,5 +77,59 @@ class MoveValidator {
     // Nước đi bắt tốt qua đường - to do
 
     return false;
+  }
+
+  // Kiểm tra logic của quân xe
+  bool _isValidRookMove(SquareModel from, SquareModel to, BoardModel board) {
+    // Quân xe chỉ di chuyển theo hàng ngang hoặc hàng dọc
+    if (from.row != to.row && from.column != to.column) {
+      return false;
+    }
+
+    // Kiểm tra xem có quân nào cản đường không
+    if (from.row == to.row) {
+      // Di chuyển theo hàng ngang
+      int start = from.column < to.column ? from.column + 1 : to.column + 1;
+      int end = from.column < to.column ? to.column : from.column;
+      for (int col = start; col < end; col++) {
+        if (board.getSquare(from.row, col)?.hasPiece == true) {
+          return false;
+        }
+      }
+    } else if (from.column == to.column) {
+      // Di chuyển theo hàng dọc
+      int start = from.row < to.row ? from.row + 1 : to.row + 1;
+      int end = from.row < to.row ? to.row : from.row;
+      for (int row = start; row < end; row++) {
+        if (board.getSquare(row, from.column)?.hasPiece == true) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  // Kiểm tra logic của quân Tượng
+  bool _isValidBishopMove(SquareModel from, SquareModel to, BoardModel board) {
+    // Quân Tượng chỉ có thể di chuyển theo hàng chéo
+    if ((from.row - to.row).abs() != (from.column - to.column).abs()) {
+      return false;
+    }
+
+    // Xác định hướng di chuyển
+    int rowDirection = (to.row - from.row) > 0 ? 1 : -1;
+    int colDirection = (to.column - from.column) > 0 ? 1 : -1;
+
+    // Kiểm ta từng ô trên đường chéo
+    int steps = (from.row - to.row).abs();
+    for (int i = 1; i < steps; i++) {
+      int intermediateRow = from.row + i * rowDirection;
+      int intermediateCol = from.column + i * colDirection;
+      if (board.getSquare(intermediateRow, intermediateCol)?.hasPiece == true) {
+        return false; // Có quân cản đường
+      }
+    }
+    return true;
   }
 }
